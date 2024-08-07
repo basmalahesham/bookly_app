b5b2e407fcf814d7fa15682eb5f9592e87e30a58
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-08 01:07:49 +0300

    create styles.dart file in utils in core and add title to home_view_body.dart

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/feature_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeatureBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureBooksListView(),
+          SizedBox(height: 50,),
+          Text('Best Seller',style: Styles.titleMedium,),
+        ],
+      ),
     );
   }
 }
