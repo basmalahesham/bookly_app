6e46840344ec8108a35f96a409bcba84eeadd8f2
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-08 02:11:50 +0300

    bestSellerListViewItem

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/feature_books_list_view.dart';
@@ -15,11 +16,49 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeatureBooksListView(),
-          SizedBox(height: 50,),
-          Text('Best Seller',style: Styles.titleMedium,),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
