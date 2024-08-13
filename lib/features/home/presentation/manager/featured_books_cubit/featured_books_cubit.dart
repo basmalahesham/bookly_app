172e67d7440b6794a71cac83fb5df1f173f741c6
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-13 20:21:20 +0300

    create newest_books_state.dart create fetchNewestBooks method in newest_books_cubit.dart newest books cubit done

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
