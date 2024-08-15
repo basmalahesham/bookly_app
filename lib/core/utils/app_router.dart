2f4aa0623682cccb963dd3e3e9ba1aadb12dba53
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-15 05:56:55 +0300

    create similar_books_cubit.dart & similar_books_state.dart & fetch similar books done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,15 +1,19 @@
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/home/presentation/views/home_view.dart';
 
-abstract class AppRouter{
+abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
   static const kSearchView = '/searchView';
-
 
   static final router = GoRouter(
     routes: [
@@ -23,13 +27,19 @@ abstract class AppRouter{
       ),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(
+            getIt.get<HomeRepoImplementation>(),
+          ),
+          child: BookDetailsView(
+            bookModel: state.extra as BookModel,
+          ),
+        ),
       ),
       GoRoute(
         path: kSearchView,
         builder: (context, state) => const SearchView(),
       ),
-
     ],
   );
 }
--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,22 +1,37 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5.0),
-          child: CustomBookImage(
-            imageUrl: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.4QY1_z2gNBFEvf3bv-BnvgHaJW%26pid%3DApi&sp=1723593453T6f22a7691ea0b09a780bb885e0d7e6cf84033ef22ed3c340e4d0c0082c7fd140',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+  builder: (context, state) {
+    if (state is SimilarBooksSuccess) {
+      return SizedBox(
+        height: MediaQuery.of(context).size.height * .15,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.0),
+            child: CustomBookImage(
+              imageUrl: 'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.4QY1_z2gNBFEvf3bv-BnvgHaJW%26pid%3DApi&sp=1723593453T6f22a7691ea0b09a780bb885e0d7e6cf84033ef22ed3c340e4d0c0082c7fd140',
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+    else if(state is SimilarBooksFailure){
+      return CustomErrorWidget(errMessage: state.errMessage);
+    }else{
+      return const CustomLoadingIndicator();
+    }
+  },
+);
   }
 }
