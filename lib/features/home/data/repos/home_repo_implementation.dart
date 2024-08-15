c46a15aa7aea8761805aaa013c23d2a07ee43dcf
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-15 05:23:47 +0300

    create & implement fetchSimilarBooks methodv

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -60,4 +60,28 @@ class HomeRepoImplementation implements HomeRepo {
       );
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?q=subject:programming&Filtering=free-ebooks&Sorting=relevance');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
