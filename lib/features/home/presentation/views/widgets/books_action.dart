5d6aa64da13a647588fcbc55157f27c734274d2a
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-16 20:09:24 +0300

    add url_launcher package in app launch url part1

--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,17 +1,20 @@
+import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 
 class BookSAction extends StatelessWidget {
-  const BookSAction({super.key});
+  const BookSAction({super.key, required this.bookModel});
 
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
-          Expanded(
+          const Expanded(
             child: CustomButton(
               text: '19.99€',
               backgroundColor: Colors.white,
@@ -24,11 +27,17 @@ class BookSAction extends StatelessWidget {
           ),
           Expanded(
             child: CustomButton(
+              onPressed: ()async{
+                Uri url = Uri.parse(bookModel.volumeInfo.previewLink!);
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                }
+              },
               text: 'Free preview',
               fontSize: 16,
-              backgroundColor: Color(0xffEF8262),
+              backgroundColor: const Color(0xffEF8262),
               textColor: Colors.white,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16),
                 bottomRight: Radius.circular(16),
               ),
