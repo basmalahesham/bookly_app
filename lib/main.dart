c2982f753e83c0f76169c1b528e6bd7e36889c15
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-21 04:49:12 +0300

    create simple_bloc_observer.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/core/utils/simple_bloc_observer.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_implementation.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
@@ -10,6 +11,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   setUpServiceLocator();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
 }
 
